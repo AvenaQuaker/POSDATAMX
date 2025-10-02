ac93ed6d820fdb347f024333a2113acf382e7eee
Jaime Aquino Martinez <jhonesbortoni@gmail.com>
2025-10-02 07:48:42 -0500

    Secciones,paleta y otras cosas

--- Middlewares/Lang.js
+++ Middlewares/Lang.js
@@ -1,9 +1,5 @@
-import translations from "../Utils/translations.js";
-
 export function detectLanguage(req) {
     const acceptLang = req.headers["accept-language"];
-    if (!acceptLang) return "en";
-
     const preferred = acceptLang.split(",")[0].split("-")[0].toLowerCase();
-    return translations[preferred] ? preferred : "en";
+    return preferred;
 }
--- Routes/Routes.js
+++ Routes/Routes.js
@@ -2,7 +2,31 @@ import { Router } from "express";
 import langs from "../Utils/translations.js";
 import { detectLanguage } from "../Middlewares/Lang.js";
 
+
 const router = Router();
+
+router.get("/Servicios", (req, res) => {
+    const lang = detectLanguage(req);
+    return res.redirect(`/Servicios/${lang}/fotografia`);
+});
+
+router.get("/Servicios/:lang/:seccion?", (req, res) => {
+    const lang = req.params.lang.toLowerCase();
+    const seccion = req.params.seccion || "fotografia";
+
+    if (!langs.sec_translations[lang]) {
+        const detected = detectLanguage(req);
+        return res.redirect(`/Servicios/${detected}/${seccion}`);
+    }
+
+    let text = {
+        nav: langs.sec_translations[lang].nav,
+        sec: langs.sec_translations[lang][seccion],
+        footer: langs.sec_translations[lang].footer
+    }
+
+    res.render("section",{ translations:text, lang });
+});
 
 router.get("/:lang", (req, res) => {
     const lang = req.params.lang.toLowerCase();
@@ -15,43 +39,10 @@ router.get("/:lang", (req, res) => {
     res.render("index", { translations: langs.translations[lang], lang });
 });
 
-router.get("/Servicios/:lang/:seccion?", (req, res) => {
-    const lang = req.params.lang.toLowerCase();
-    const seccion = req.params.seccion || "fotografia";
-
-    if (!langs.sec_translations[lang]) {
-        const detected = detectLanguage(req);
-        console.log(detected);
-        return res.redirect(`/Servicios/${detected}/${seccion}`);
-    }
-
-    switch (seccion.toLowerCase()) {
-        case 'fotografia':
-            return res.render("section1", { translations: langs.sec_translations[lang], lang });
-        case 'diseno':
-            return res.render("section2", { translations: langs.sec_translations[lang], lang });
-        case 'multimedia':
-            return res.render("section3", { translations: langs.sec_translations[lang], lang });
-        case 'hosting':
-            return res.render("section4", { translations: langs.sec_translations[lang], lang });
-        case 'publicidad':
-            return res.render("section5", { translations: langs.sec_translations[lang], lang });
-        default:
-            return res.render("section1", { translations: langs.sec_translations[lang], lang });
-    }
-});
-
 
 router.get("/", (req, res) => {
     const lang = detectLanguage(req);
     res.redirect(`/${lang}`);
 });
-
-router.get("/Servicios", (req, res) => {
-    const lang = detectLanguage(req);
-    console.log(lang);
-    return res.redirect(`/Servicios/es/`);
-});
-
 
 export default router;
